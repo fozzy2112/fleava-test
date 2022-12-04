be1910d341fc71b13659d4fdb2f46a65246c3642
fozzy2112 <difiorahmandha21@gmail.com>
2022-12-04 16:07:23 +0700

    feat(implement caraousel)

---
+++ components/Header/header.vue
@@ -0,0 +1,51 @@
+<template>
+	<section>
+		<h2>Flickity</h2>
+		<flickity  class="flickity" ref="flickity" :options="flickityOptions">
+			<div class="carousel-cell">
+				<img src="https://via.placeholder.com/150" style="width:100%" alt="">
+			</div>
+			<div class="carousel-cell">
+				<img src="https://via.placeholder.com/150" style="width:100%" alt="">
+			</div>
+			<div class="carousel-cell">
+				<img src="https://via.placeholder.com/150" style="width:100%" alt="">
+			</div>
+			<div class="carousel-cell">
+				<img src="https://via.placeholder.com/150" style="width:100%" alt="">
+			</div>
+			<div class="carousel-cell">
+				<img src="https://via.placeholder.com/150" style="width:100%" alt="">
+			</div>
+		</flickity>
+	</section>
+</template>
+
+<script>
+import Flickity from 'vue-flickity'
+export default {
+	components : {
+		Flickity
+	},
+	data() {
+		return {
+			flickityOptions: {
+				initialIndex: 3,
+				prevNextButtons: true,
+				pageDots: true,
+				wrapAround: true,
+				freeScroll: true
+				// any options from Flickity can be used
+			}
+		}
+	}
+}
+</script>
+
+<style>
+.carousel-cell {
+  width: 100%; /* full width */
+  height: 160px; /* height of carousel */
+  margin-right: 10px;
+}
+</style>
---
+++ components/MainPage.vue
@@ -0,0 +1,12 @@
+<template>
+	<Header />
+</template>
+
+<script>
+import Header from '../components/Header/header.vue';
+export default {
+	components: {
+		Header
+	}
+}
+</script>
